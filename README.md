6835ab055f35619aaca87285cb4403b7b619a27c
rovin0805 <qhdrn67@naver.com>
2020-12-05 18:02:29 +0900

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# React Board
-
-Basic Board made by React
-
-## Result
-
-[https://rovin0805.github.io/reactBoard/]
---
+++ precache-manifest.7e9c3a91206ee732be5cfa2d62eadd7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a16f812a6799858e3abbe939d924ac",
+    "url": "/reactBoard/index.html"
+  },
+  {
+    "revision": "6e99cd96d32921dd921d",
+    "url": "/reactBoard/static/js/2.6152e6ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactBoard/static/js/2.6152e6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e3cba580e57c590986",
+    "url": "/reactBoard/static/js/main.661619f9.chunk.js"
+  },
+  {
+    "revision": "dec2a335af13bdf65f16",
+    "url": "/reactBoard/static/js/runtime-main.4b13d29d.js"
+  }
+]);
